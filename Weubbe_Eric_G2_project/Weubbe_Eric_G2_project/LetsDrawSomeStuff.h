b53e24a23bcde523081f03d978c922de50e6690d
Eric Weubbe <EJWeubbe@student.fullsail.edu>
2019-01-15 23:25:10 -0500

    updated view matrix

--- Weubbe_Eric_G2_project/Weubbe_Eric_G2_project/LetsDrawSomeStuff.h
+++ Weubbe_Eric_G2_project/Weubbe_Eric_G2_project/LetsDrawSomeStuff.h
@@ -250,7 +250,7 @@ LetsDrawSomeStuff::LetsDrawSomeStuff(GW::SYSTEM::GWindow* attatchPoint)
 			worldM = XMMatrixIdentity();
 
 			//Initialize the view matrix
-			XMVECTOR Eye = XMVectorSet(0.0f, 5.0f, -10.0f, 0.0f);
+			XMVECTOR Eye = XMVectorSet(0.0f, 7.0f, -7.0f, 0.0f);
 			XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 			XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 			viewM = XMMatrixLookAtLH(Eye, At, Up);
